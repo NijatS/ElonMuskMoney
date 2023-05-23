888fad9a331a7e2651b21dc5b7a51cbaf8f6f6b1
NijatS <nijatjs@code.edu.az>
2023-05-23 23:08:26 +0400

    fix:input

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ products.forEach((product) => {
   const sellBtn = product.querySelector(".buySell >button:first-child");
   const value = product.querySelector("span").textContent.split(",").join("");
   const amountProduct = product.querySelector(".buySell >input");
-
+  let deyer = 0;
   sellBtn.disabled = true;
 
   document.body.addEventListener("click", () => {
@@ -29,7 +29,7 @@ products.forEach((product) => {
     } else {
       buyBtns = [];
     }
-    console.log(buyBtns);
+    // console.log(buyBtns);
   });
   buyBtn.addEventListener("click", () => {
     console.log("buy");
@@ -53,40 +53,72 @@ products.forEach((product) => {
     }
     info();
   });
-  /*let eventList = ["change", "keyup"];
-  for (event of eventList) {
-    amountProduct.addEventListener(event, () => {
-      //  EnableButton();
-      //let total = Number(totalMoney.textContent.split(",").join(""));
-      if (amountProduct.value > 0) {
-        sellBtn.disabled = false;
-        sellBtn.style.backgroundColor = "rgb(214, 48, 49)";
-      } else {
-        sellBtn.disabled = true;
-        sellBtn.style.backgroundColor = "rgb(220, 220, 220)";
-      }
-      if (amountProduct.value < 0) {
-        amountProduct.value = 0;
-      }
-      if (total < value) {
-        buyBtns.forEach((button) => {
-          button.disabled = true;
-          button.style.backgroundColor = "rgb(220, 220, 220)";
-          // amountProduct.value = Math.trunc(totalMoneyC / value);
-        });
-      }
-      //totalMoney.textContent = "187,000,000,000";
-      // total = Number(totalMoney.textContent.split(",").join(""));
-      const totalProductMoney = amountProduct.value * value;
-      if (amountProduct.value * value > total) {
-        amountProduct.value = Math.trunc(total / value);
-      }
-      total = Number(total) - Number(totalProductMoney);
+  // let eventList = ["change", "keyup"];
+  // for (event of eventList) {
+  //   amountProduct.addEventListener(event, () => {
+  //     // EnableButton();
+  //     // let total = Number(totalMoney.textContent.split(",").join(""));
+  //     if (amountProduct.value > 0) {
+  //       sellBtn.disabled = false;
+  //       sellBtn.style.backgroundColor = "rgb(214, 48, 49)";
+  //     } else {
+  //       sellBtn.disabled = true;
+  //       sellBtn.style.backgroundColor = "rgb(220, 220, 220)";
+  //     }
+  //     if (amountProduct.value < 0) {
+  //       amountProduct.value = 0;
+  //     }
+  //     if (total < value) {
+  //       buyBtns.forEach((button) => {
+  //         button.disabled = true;
+  //         button.style.backgroundColor = "rgb(220, 220, 220)";
+  //         amountProduct.value = Math.trunc(totalMoneyC / value);
+  //       });
+  //     }
+  //     // totalMoney.textContent = "187,000,000,000";
+  //     //total = Number(totalMoney.textContent.split(",").join(""));
+  //     const totalProductMoney = amountProduct.value * value;
+  //     if (amountProduct.value * value > total) {
+  //       amountProduct.value = Math.trunc(total / value);
+  //     }
+  //     total = Number(total) - Number(totalProductMoney);
+  //     totalMoney.textContent = numberWithCommas(total);
+  //     info();
+  //   });
+  //   return;
+  // }
+  amountProduct.addEventListener("change", () => {
+    if (amountProduct.value > 0) {
+      sellBtn.disabled = false;
+      sellBtn.style.backgroundColor = "rgb(214, 48, 49)";
+    } else {
+      sellBtn.disabled = true;
+      sellBtn.style.backgroundColor = "rgb(220, 220, 220)";
+    }
+    if (amountProduct.value < 0) {
+      amountProduct.value = 0;
+    }
+    total += deyer;
+    // total += amountProduct.value * value;
+    const totalProductMoney = amountProduct.value * value;
+    deyer = totalProductMoney;
+    //total += totalProductMoney;
+    if (amountProduct.value * value > total) {
+      amountProduct.value = Math.trunc(total / value);
+      total -= amountProduct.value * value;
       totalMoney.textContent = numberWithCommas(total);
-      info();
-    });
-    return;
-  }*/
+      return;
+    }
+    total -= Number(totalProductMoney);
+    totalMoney.textContent = numberWithCommas(total);
+    console.log(deyer);
+    info();
+  });
+  amountProduct.addEventListener("keydown", (e) => {
+    if (e.which === 38 || e.which === 40) {
+      e.preventDefault();
+    }
+  });
 });
 function numberWithCommas(data) {
   return data.toLocaleString();
