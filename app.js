9810b1f71b15b6adc87d7fdce13fc79b35404715
NijatS <nijatjs@code.edu.az>
2023-05-23 20:10:15 +0400

    fix:Buy Buttons

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const totalMoney = document.querySelector(".totalMoney >h1 >span");
 const products = document.querySelectorAll(".product");
 const receipt = document.querySelector(".receipt");
+const buyBtns = document.querySelectorAll(".buySell >button:last-child");
+const totalMoneyC = 187000000000;
 
 products.forEach((product) => {
   const buyBtn = product.querySelector(".buySell >button:last-child");
@@ -10,19 +12,26 @@ products.forEach((product) => {
 
   sellBtn.disabled = true;
   buyBtn.addEventListener("click", () => {
+    EnableButton();
     amountProduct.value++;
     let total = Number(totalMoney.textContent.split(",").join(""));
-    // let totalProductMoney = amountProduct.value * value;
     total = total - value;
     totalMoney.textContent = numberWithCommas(total);
     sellBtn.disabled = false;
     sellBtn.style.backgroundColor = "rgb(214, 48, 49)";
+    if (total < value) {
+      buyBtns.forEach((button) => {
+        button.disabled = true;
+        amountProduct.value = Math.trunc(totalMoneyC / value);
+        button.style.backgroundColor = "rgb(220, 220, 220)";
+      });
+    }
     info();
   });
   sellBtn.addEventListener("click", () => {
+    EnableButton();
     let total = Number(totalMoney.textContent.split(",").join(""));
     amountProduct.value--;
-    //let totalProductMoney = amountProduct.value * value;
     total = Number(total) + Number(value);
     totalMoney.textContent = numberWithCommas(total);
     if (amountProduct.value == 0) {
@@ -34,6 +43,7 @@ products.forEach((product) => {
   let eventList = ["change", "keyup"];
   for (event of eventList) {
     amountProduct.addEventListener(event, () => {
+      EnableButton();
       let total = Number(totalMoney.textContent.split(",").join(""));
       if (amountProduct.value > 0) {
         sellBtn.disabled = false;
@@ -42,9 +52,15 @@ products.forEach((product) => {
         sellBtn.disabled = true;
         sellBtn.style.backgroundColor = "rgb(220, 220, 220)";
       }
-
       if (amountProduct.value < 0) {
         amountProduct.value = 0;
+      }
+      if (total < value) {
+        buyBtns.forEach((button) => {
+          button.disabled = true;
+          button.style.backgroundColor = "rgb(220, 220, 220)";
+          amountProduct.value = Math.trunc(totalMoneyC / value);
+        });
       }
       totalMoney.textContent = "187,000,000,000";
       total = Number(totalMoney.textContent.split(",").join(""));
@@ -92,10 +108,9 @@ function info() {
   totalAmountReceipt.textContent = "$" + numberWithCommas(total);
 }
 
-/*  if (total < value) {
-    buyBtn.disabled = true;
-    buyBtn.style.backgroundColor = "rgb(220, 220, 220)";
-  } else {
-    buyBtn.disabled = false;
-    buyBtn.style.backgroundColor = "rgb(162, 155, 254)";
-  }*/
+function EnableButton() {
+  buyBtns.forEach((button) => {
+    button.disabled = false;
+    button.style.backgroundColor = "rgb(162, 155, 254)";
+  });
+}
